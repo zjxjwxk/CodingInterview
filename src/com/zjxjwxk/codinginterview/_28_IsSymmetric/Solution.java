79c408e8b8e4edebc61e78f42510e394e9aecd00
zjxjwxk <zjxjwxk@gmail.com>
2020-09-26 21:46:36 +0800

    add 28

---
+++ src/com/zjxjwxk/codinginterview/_28_IsSymmetric/Solution.java
@@ -0,0 +1,48 @@
+package com.zjxjwxk.codinginterview._28_IsSymmetric;
+
+/**
+ * 剑指 Offer 28. 对称的二叉树
+ *
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树
+ * 和它的镜像一样，那么它是对称的。
+ *
+ * 示例 1：
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ *
+ * 限制：
+ * 0 <= 节点个数 <= 1000
+ *
+ * @author zjxjwxk
+ * @date 2020/9/26 9:33 下午
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+
+    private boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        return root1.val == root2.val && isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
