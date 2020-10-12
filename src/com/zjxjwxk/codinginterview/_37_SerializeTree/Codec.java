47b48477b37f03fe883b7e2dfa5dd469cbb2d633
zjxjwxk <zjxjwxk@gmail.com>
2020-10-12 15:45:24 +0800

    Add comment

--- src/com/zjxjwxk/codinginterview/_37_SerializeTree/Codec.java
+++ src/com/zjxjwxk/codinginterview/_37_SerializeTree/Codec.java
@@ -4,6 +4,17 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 /**
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ *
+ * 示例: 
+ * 你可以将以下二叉树：
+ *     1
+ *    / \
+ *   2   3
+ *      / \
+ *     4   5
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ *
  * @author zjxjwxk
  * @date 2020/10/12 2:02 下午
  */
